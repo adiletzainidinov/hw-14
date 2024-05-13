0a7f2defa0664a36eda01d82f34071dadb4d8bf8
Nursultan Kenzhebaev <nursultan@beeworx.de>
2024-05-13 08:13:25 +0600

    part 2

---
+++ src/utils/general.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    id: "1",
+    title: "Sushi",
+    price: 22.99,
+    amount: 3,
+  },
+  {
+    id: "2",
+    title: "Schnitzel",
+    price: 16.0,
+    amount: 3,
+  },
+  {
+    id: "3",
+    title: "Barbecue Burger",
+    price: 12.99,
+    amount: 3,
+  },
+  {
+    id: "4",
+    title: "Green Bowl",
+    price: 19.99,
+    amount: 3,
+  },
+];
